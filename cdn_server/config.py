1c3c3f2b0d5961b2d52b1897eddda98525205019
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-06-04 01:50:13 +0530

    Deploy build

--- cdn_server/config.py
+++ cdn_server/config.py
@@ -1,9 +1,11 @@
+import os
 # Default configurations:
 config = {
     # "BASE_DIR": "/home/supratik/Documents/data-attached/space/projects/conceivilize_site/git-server/test/",
     "SERVER_NAME": "minjscdn.herokuapp.com",
     # "SQLALCHEMY_DATABASE_URI": "postgres+psycopg2://workbench:workbench@localhost:5432/workbench",
-    "SQLALCHEMY_DATABASE_URI": 'postgresql://cdfhkfxs:PynQ9IKe_Yce9S4xgs2s-ji2dMB5N3Kg@john.db.elephantsql.com/cdfhkfxs',
+    # "SQLALCHEMY_DATABASE_URI": 'postgresql://cdfhkfxs:PynQ9IKe_Yce9S4xgs2s-ji2dMB5N3Kg@john.db.elephantsql.com/cdfhkfxs',
+    "SQLALCHEMY_DATABASE_URI": "sqlite:///"+os.path.join(os.getcwd(), 'random.sqlite'),
     "SQLALCHEMY_BINDS": {},
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
     "PERMANANENT_SESSION_LIFETIME": 7776000.0,
