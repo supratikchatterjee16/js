b6bb082226e8f97f86bdc7d47b3ffee89bd73e69
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-06-04 00:44:28 +0530

    Deploy build

--- cdn_server/core/routes/__init__.py
+++ cdn_server/core/routes/__init__.py
@@ -7,11 +7,18 @@ from flask import Blueprint, send_from_directory, abort
 
 import os
 main = Blueprint('main', __name__, template_folder='templates')
+
+
+@main.get('/')
+def send_welcome():
+    return 'This is a simple CDN for increasing visibility of JS scripts prepared by Conceivilize as a FOSS'
+
+
 @main.get('/<script>')
-def serve_script(script : str= ''):
+def serve_script(script: str = ''):
     if script == None or script == '':
         abort(404)
     #base_url = 'https://raw.githubusercontent.com/supratikchatterjee16/js/master/jsbuilds/'
-    script_filename = script +'.min.js'
+    script_filename = script + '.min.js'
     print(os.getcwd())
     return send_from_directory(os.path.join(os.getcwd(), 'jsbuilds'), script_filename)
