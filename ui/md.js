9d78efd84b4cfefc5e3b5d4d558056b0a2f09e41
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-07-27 17:30:36 +0530

    Deploy build

--- ui/md.js
+++ ui/md.js
@@ -2,32 +2,26 @@ class MDView extends CustomElement {
     constructor() {
         super();
     }
-    
     format(content){
-        
+        this.innerHTML = mdHtml.render(content);
     }
-    
     connectedCallback() {
         let source = this.getAttribute("src");
         if(!window.markdownit){
             console.error("Markdown-It was not loaded.\nAdd : <script src=\"https://cdnjs.cloudflare.com/ajax/libs/markdown-it/13.0.1/markdown-it.min.js\"></script>\nAborting.");
-            console.info("We depend on Markdown It for the official implementation of Markdown rendering in JS. We aren't adding this, as you(the developer) need to implement it by yourself.");
+            console.info("We depend on Markdown It for the official implementation of Markdown rendering in JS. We aren't adding this, as you(the developer) need to implement it by yourself.\n Furthermore for syntax highlighting, hljs is used.");
             return;
         }
-        else{// the below has been adapted from https://markdown-it.github.io/index.js
-            if (defaults._strict) {
-                mdHtml = window.markdownit("commonmark");
-                mdSrc = window.markdownit("commonmark");
-            } else if(!MDView.setup){
-                mdHtml = window.markdownit(defaults).use(markdownItAbbr).use(markdownItContainer, "warning").use(markdownItDeflist).use(markdownItEmoji).use(markdownItFootnote).use(markdownItIns).use(markdownItMark).use(markdownItSub).use(markdownItSup);
-                mdSrc = window.markdownit(defaults).use(markdownItAbbr).use(markdownItContainer, "warning").use(markdownItDeflist).use(markdownItEmoji).use(markdownItFootnote).use(markdownItIns).use(markdownItMark).use(markdownItSub).use(markdownItSup);
+        else if(!MDView.setup){// the below has been adapted from https://markdown-it.github.io/index.js
+            this.mdHtml = window.markdownit("commonmark");
+            this.mdSrc = window.markdownit("commonmark");
             }
             // Beautify output of parser for html content
-            mdHtml.renderer.rules.table_open = function() {
+            this.mdHtml.renderer.rules.table_open = function() {
                 return '<table class="table table-striped">\n';// NOTE : Bootstrap usage. Remove.
             };
             // Replace emoji codes with images
-            mdHtml.renderer.rules.emoji = function(token, idx) {
+            this.mdHtml.renderer.rules.emoji = function(token, idx) {
                 return window.twemoji.parse(token[idx].content);// NOTE : Twitter Emoji pack. Throw out.
             };
             
@@ -40,7 +34,7 @@ class MDView extends CustomElement {
                 }
                 return slf.renderToken(tokens, idx, options, env, slf);
             }
-            mdHtml.renderer.rules.paragraph_open = mdHtml.renderer.rules.heading_open = injectLineNumbers;
+            this.mdHtml.renderer.rules.paragraph_open = this.mdHtml.renderer.rules.heading_open = injectLineNumbers;
             
             MDView.setup = true;// injected to perform singleton action
         }
