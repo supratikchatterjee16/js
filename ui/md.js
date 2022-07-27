417315986bd0893e48918cf06e38fcad8da9920d
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-07-27 14:35:14 +0530

    Deploy build

--- ui/md.js
+++ ui/md.js
@@ -6,7 +6,7 @@ class MDView extends CustomElement {
     format(content){
         console.log(content);
         content.split('\n').forEach(line => {
-            if (line.startswith("#")) { // headings
+            if (line.startsWith("#")) { // headings
                 let level = 0;
                 for (level = 0; level < line.length; level++)
                     if (line.charAt(level) != '#' || level == 6)// h6 is the ast header level in html
@@ -15,22 +15,22 @@ class MDView extends CustomElement {
                 element.innerHTML = line.substring(level).trim();
                 this.appendChild(element);
             }
-            else if (line.startswith("*")) { // italic and bold
+            else if (line.startsWith("*")) { // italic and bold
 
             }
-            else if (line.startswith(">")) { // blockquote
+            else if (line.startsWith(">")) { // blockquote
 
             }
-            else if (line.startswith("-")) {// unordered list(- )/horizontal rule(---)/tasklist(- [ ])
+            else if (line.startsWith("-")) {// unordered list(- )/horizontal rule(---)/tasklist(- [ ])
 
             }
-            else if (line.startswith("`") || line.startswth('~')) {// code or code block(syntax highlighting to be added later)/ strikethrough(~~)
+            else if (line.startsWith("`") || line.startswth('~')) {// code or code block(syntax highlighting to be added later)/ strikethrough(~~)
 
             }
-            else if (line.startswith("[")) {// expect link/ or footnote([^x])
+            else if (line.startsWith("[")) {// expect link/ or footnote([^x])
 
             }
-            else if (line.startswith("![")) {// expect image
+            else if (line.startsWith("![")) {// expect image
 
             }
             else if (line.match(/^\d/)) {// ordered list
